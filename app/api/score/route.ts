2a461398485798da6c0240e5687e7fdf2897cfea
fraolb <Fraolbereket@gmail.com>
2025-06-11 19:00:42 +0300

    update env variable

--- app/api/score/route.ts
+++ app/api/score/route.ts
@@ -49,12 +49,12 @@ export async function POST(request: Request) {
         { status: 401 }
       );
     }
-    if (!process.env.JWT_SECRET) {
+    if (!process.env.NEXT_PUBLIC_JWT_SECRET) {
       throw new Error("JWT_SECRET not configured");
     }
 
     const token = authHeader.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+    const decoded = jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET!) as {
       username: string;
       score: number;
     };
--- lib/gameAuth.ts
+++ lib/gameAuth.ts
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const createScoreToken = (username: string, score: number) => {
-  if (!process.env.JWT_SECRET) {
+  if (!process.env.NEXT_PUBLIC_JWT_SECRET) {
     throw new Error("JWT_SECRET not configured from createScoreToken");
   }
-  return jwt.sign({ username, score }, process.env.JWT_SECRET!, {
+  return jwt.sign({ username, score }, process.env.NEXT_PUBLIC_JWT_SECRET!, {
     expiresIn: "2m",
   });
 };
