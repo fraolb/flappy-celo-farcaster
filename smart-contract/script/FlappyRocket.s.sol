09b37dc82cb240dc943720df5b07572c3f5ca056
fraolb <Fraolbereket@gmail.com>
2025-06-18 22:08:05 +0300

    game smart contract

---
+++ smart-contract/script/FlappyRocket.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import {FlappyRocketGameTest} from "../src/FlappyRocket.sol";
+
+contract DeployFlappyRocketWithArgs is Script {
+    function run(uint256 minDeposit) external {
+        require(minDeposit > 0, "Minimum deposit must be > 0");
+
+        vm.startBroadcast();
+
+        FlappyRocketGameTest game = new FlappyRocketGameTest(minDeposit);
+
+        vm.stopBroadcast();
+
+        console.log("FlappyRocketGame deployed at:", address(game));
+        console.log("Minimum deposit set to:", minDeposit, "wei");
+        console.log("Owner:", game.owner());
+    }
+}
