e317a0e2f8057c5f4e2672d4f27ccbe3a9de13f6
fraolb <Fraolbereket@gmail.com>
2025-08-19 14:58:34 +0300

    add default values for the model

--- model/userPlays.ts
+++ model/userPlays.ts
@@ -7,8 +7,8 @@ const UserPlays: Schema = new Schema(
     wallet: { type: String, unique: true },
     playsLeft: { type: Number, default: 4 },
     lastPlay: Date,
-    lastEarned: Number,
-    totalEarned: Number, // For analytics
+    lastEarned: { type: Number, default: 0 },
+    totalEarned: { type: Number, default: 0 }, // For analytics
   },
   { timestamps: true }
 );
