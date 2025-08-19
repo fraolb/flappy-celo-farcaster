8ec8c3519adba4eb1f64c68af8ebd906b18381eb
fraolb <Fraolbereket@gmail.com>
2025-08-19 15:28:21 +0300

    add generate token for game play

--- lib/gameAuth.ts
+++ lib/gameAuth.ts
@@ -1,6 +1,9 @@
 import { SignJWT } from "jose";
 
-export const createScoreToken = async (username: string, score: number) => {
+export const createScoreToken = async (
+  username: string | undefined,
+  score: number
+) => {
   if (!process.env.NEXT_PUBLIC_JWT_SECRET) {
     throw new Error("JWT secret not configured");
   }
@@ -11,6 +14,31 @@ export const createScoreToken = async (username: string, score: number) => {
     return await new SignJWT({
       username,
       score,
+    })
+      .setProtectedHeader({ alg: "HS256" })
+      .setIssuedAt()
+      .setExpirationTime("2m")
+      .sign(secret);
+  } catch (error) {
+    console.error("Token signing failed:", error);
+    throw error;
+  }
+};
+
+export const generatePlayToken = async (
+  address: string,
+  username: string | undefined
+) => {
+  if (!process.env.NEXT_PUBLIC_JWT_SECRET) {
+    throw new Error("JWT secret not configured");
+  }
+
+  try {
+    const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
+
+    return await new SignJWT({
+      address,
+      username,
     })
       .setProtectedHeader({ alg: "HS256" })
       .setIssuedAt()
