a01f236ff673ac61f8743abd88ba35268708db30
fraolb <Fraolbereket@gmail.com>
2025-09-23 11:02:27 +0300

    remove divvi

--- app/api/reward/route.ts
+++ app/api/reward/route.ts
@@ -3,7 +3,7 @@ import { createPublicClient, createWalletClient, http } from "viem";
 import { celo } from "viem/chains";
 import { privateKeyToAccount } from "viem/accounts";
 import { encodeFunctionData, parseEther } from "viem";
-import { submitReferral, getReferralTag } from "@divvi/referral-sdk";
+// import { submitReferral, getReferralTag } from "@divvi/referral-sdk";
 import FlappyRocketGameABI from "../../../ABI/FlappyRocket.json";
 
 import UserPlay from "@/model/userPlays";
@@ -40,16 +40,16 @@ export async function POST(request: Request) {
       transport: http("https://forno.celo.org"),
     });
 
-    let referralTag;
-    try {
-      referralTag = getReferralTag({
-        user: "0x137f7b4Ac3BB0456C08A1025335133FF3120844f" as `0x${string}`,
-        consumer: "0xC00DA57cDE8dcB4ED4a8141784B5B4A5CBf62551",
-      });
-    } catch (diviError) {
-      console.error("Divvi getDataSuffix error:", diviError);
-      throw new Error("Failed to generate referral data");
-    }
+    // let referralTag;
+    // try {
+    //   referralTag = getReferralTag({
+    //     user: "0x137f7b4Ac3BB0456C08A1025335133FF3120844f" as `0x${string}`,
+    //     consumer: "0xC00DA57cDE8dcB4ED4a8141784B5B4A5CBf62551",
+    //   });
+    // } catch (diviError) {
+    //   console.error("Divvi getDataSuffix error:", diviError);
+    //   throw new Error("Failed to generate referral data");
+    // }
 
     // Validate and setup sponsor account
     const privateKey = process.env.SPONSOR_PRIVATE_KEY;
@@ -74,15 +74,15 @@ export async function POST(request: Request) {
       args: [wallet, rewardInBigint],
     });
 
-    const combinedData = referralTag
-      ? contractData + referralTag
-      : contractData;
+    // const combinedData = referralTag
+    //   ? contractData + referralTag
+    //   : contractData;
 
     // Send transaction with the minimum deposit value
     const hash = await walletClient.sendTransaction({
       account,
       to: FlappyRocketGameAddress,
-      data: combinedData as `0x${string}`,
+      data: contractData as `0x${string}`,
       value: parseEther("0"),
     });
 
@@ -111,16 +111,16 @@ export async function POST(request: Request) {
     }
 
     // Optional: Report to Divi
-    if (referralTag) {
-      try {
-        await submitReferral({
-          txHash: receipt.transactionHash,
-          chainId: 42220,
-        });
-      } catch (diviError) {
-        console.error("Divi referral error:", diviError);
-      }
-    }
+    // if (referralTag) {
+    //   try {
+    //     await submitReferral({
+    //       txHash: receipt.transactionHash,
+    //       chainId: 42220,
+    //     });
+    //   } catch (diviError) {
+    //     console.error("Divi referral error:", diviError);
+    //   }
+    // }
 
     return NextResponse.json({
       success: true,
